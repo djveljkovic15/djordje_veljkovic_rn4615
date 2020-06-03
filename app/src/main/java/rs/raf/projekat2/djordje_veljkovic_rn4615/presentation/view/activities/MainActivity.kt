90320b7dd9c9c8eb33f6d4490d9119e9f663e48d
dzo <djveljkovic15@raf.rs>
2020-06-03 17:34:39 +0200

    Porukica

---
+++ app/src/main/java/rs/raf/projekat2/djordje_veljkovic_rn4615/presentation/view/activities/MainActivity.kt
@@ -0,0 +1,19 @@
+package rs.raf.projekat2.djordje_veljkovic_rn4615.presentation.view.activities
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import rs.raf.projekat2.djordje_veljkovic_rn4615.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        init()
+    }
+    private fun init(){
+
+
+    }
+
+}
